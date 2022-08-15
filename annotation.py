58f5a3554df1f467588b5cc37a87e0ed765010bb
John Moore <john.moore@nationalarchives.gov.uk>
2022-08-15 14:59:13 +0100

    valid the annotation uri for redaction

--- annotation.py
+++ annotation.py
@@ -1,19 +1,22 @@
 from miiify import Miiify
 from gh import Repository
 
+
 class Annotation:
     def __init__(self, ctx):
         self.miiify = Miiify(ctx)
         self.repo = Repository(ctx)
+        self.logger = ctx.logger
+        self.container = ctx.container
 
     def clone(self, ctx):
         self.repo.clone(ctx)
         return f"cloned {ctx.remote_repo} to {ctx.local_repo}"
 
-
     def create_container(self, ctx):
         self.miiify.create_container("A Container for Miiifybot")
-        self.repo.pull_request("Miiifybot", f"create {ctx.container} container")
+        self.repo.pull_request(
+            "Miiifybot", f"create {ctx.container} container")
         return f"Container {ctx.container} created"
 
     def __delete_annotation(self, id):
@@ -21,14 +24,34 @@ class Annotation:
         self.repo.pull_request("Miiifybot", f"delete {id} annotation")
         return f"Annotation {id} deleted"
 
+    def __is_annotation(self, uri):
+        match uri.split('/'):
+            case [proto, '', host, 'annotations', self.container, annotation]:
+                return True
+            case _ as uri:
+                return False
 
     def redact(self, dict):
-        desc = dict['description']
-        lis = desc.split('\n')
-        for id in lis:
-            self.__delete_annotation(id.strip())
-        return "redacted content"
-
+        try:
+            desc = dict['description']
+            lis = desc.split('\n')
+            complete = True
+            for id in lis:
+                if self.__is_annotation(id):
+                    self.__delete_annotation(id.strip())
+                else:
+                    complete = False
+                    self.logger.info(f"Ignoring {id} as it does not appear to be a valid annotation uri")
+        except:
+            self.logger.error('was not able to parse the embeds content')
+            return "burp!"
+        else:
+            if complete == True:
+                self.logger.info('redacted all the annotations supplied')
+                return "redacted content"
+            else:
+                self.logger.error('did not redact all the annotations')
+                return "burp!"
 
     def describe(self, author, content):
         lis = content.split(' ')
@@ -38,11 +61,8 @@ class Annotation:
         self.repo.pull_request("Miiifybot", f"discord user {author}")
         return f"{author} submitted an annotation for review"
 
-
     def about(self, content):
         lis = content.split(' ')
         item = lis[1]
         res = self.miiify.read_annotation(item)
         return res
-
-    
